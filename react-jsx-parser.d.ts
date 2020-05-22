1cba289dc5d07465f36d1f8f689dbd2bfe34b754
vojtechvportes <vojtech.v.portes@gmail.com>
2020-05-22 20:16:25 +0200

    Updated typings

--- react-jsx-parser.d.ts
+++ react-jsx-parser.d.ts
@@ -1,1 +1,40 @@
-declare module "react-jsx-parser";
+
+export interface JsxParserProps {
+  allowUnknownElements?: boolean;
+  bindings?: any;
+  blacklistedAttrs?: any[];
+  blacklistedTags?: string[];
+  components?: any;
+  componentsOnly?: boolean;
+  disableFragments?: boolean;
+  disableKeyGeneration?: boolean;
+  jsx?: string;
+  onError?: (error: any) => void;
+  showWarnings?: boolean;
+  renderError?: (error: string) => any;
+  renderInWrapper?: boolean;
+}
+
+export default class JsxParser extends React.Component<JsxParserProps> {
+  static displayName: string;
+  static defaultProps: {
+      allowUnknownElements: boolean;
+      bindings: {};
+      blacklistedAttrs: RegExp[];
+      blacklistedTags: string[];
+      components: never[];
+      componentsOnly: boolean;
+      disableFragments: boolean;
+      disableKeyGeneration: boolean;
+      jsx: string;
+      onError: () => void;
+      showWarnings: boolean;
+      renderError: undefined;
+      renderInWrapper: boolean;
+  };
+  parseJSX: (rawJSX: any) => any;
+  parseExpression: (expression: any) => any;
+  parseName: (element: any) => any;
+  parseElement: (element: any) => any;
+  render(): JSX.Element;
+}
